477155ea681f97c2c52748e28511f650b8407db4
Matthew <68511341+Mat0uz@users.noreply.github.com>
2025-06-22 23:22:30 -0400

    Update quizcal.cpp

--- quizcal.cpp
+++ quizcal.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <iomanip> // For setprecision anf setw
+#include <iomanip>
 using namespace std;
 
 int main() {
@@ -9,33 +9,233 @@ int main() {
     string format;
     float quiz1, quiz2, quiz3, quiz4;
     float lowest;
-    
-    // Get user input
-    cout << "Students name: ";
-    cin >> name;
-    cout << "Are the grades in precentage or 10-points format?(enter percent or points):";
-    cin >> format;
-    cout << "Examples of entered grades.\n" << "Precentage: 80.5 99 100 0\n" 
-         << "Points: 5 9.3 9 10\n" << "Enter all four quiz grades in (separated by spaces): ";
-    cin >> quiz1 >> quiz2 >> quiz3 >> quiz4;
 
+    // Get user input
+
+    cout << "Welcome to CSC 126 quiz average calculator!\n"
+    << "This program adds up 4 quiz scores and removes the lowest from the final grade\n" <<
+    "Please enter student's name to begin: ";
+    cin >> name;
+    cout << "Are the grades in percentage or 10-points format?(enter percent or points): ";
+    cin >> format;
+    
+    //Invalid input loop for selecting format
+    
+    while (format != "percent" && format != "points")
+    {
+          cout << "Invalid input!\n" << "Enter percent or points: ";
+          cin >> format;
+    }
+    
+    cout << "Enter all four quiz scores(separated by spaces): ";
+    cin >> quiz1 >> quiz2 >> quiz3 >> quiz4;
+    
+    //Invalid input loop for grades
+    
+    if (format == "points")
+        while (quiz1 > 10 || quiz1 < 0 || quiz2 > 10 || quiz2 < 0 || quiz3 > 10 || quiz3 < 0 || quiz4 > 10 || quiz4 < 0)
+        {
+            cout << "Invalid Input!\n" << "Separate scores with spaces(Ex. 10 5.5 9 7): ";   
+            cin >> quiz1 >> quiz2 >> quiz3 >> quiz4;
+        }
+    else 
+         while (quiz1 > 100 && quiz1 < 0 && quiz2 > 100 && quiz2 < 0 && quiz3 > 100 && quiz3 < 0 && quiz4 > 100 && quiz4 < 0)
+        {
+            cout << "Invalid Input!\n" << "Separate scores with spaces(Ex. 100 55.5 90 77): ";   
+            cin >> quiz1 >> quiz2 >> quiz3 >> quiz4;
+        }
+     
     // Determine the lowest score
-    if (quiz1 <= quiz2 && quiz1 <= quiz3 && quiz1 <= quiz4) 
+    if (quiz1 <= quiz2 && quiz1 <= quiz3 && quiz1 <= quiz4)
         lowest = quiz1;
-    else if (quiz2 <= quiz1 && quiz2 <= quiz3 && quiz2 <= quiz4) 
+    else if (quiz2 <= quiz1 && quiz2 <= quiz3 && quiz2 <= quiz4)
         lowest = quiz2;
-    else if (quiz3 <= quiz1 && quiz3 <= quiz2 && quiz3 <= quiz4) 
+    else if (quiz3 <= quiz1 && quiz3 <= quiz2 && quiz3 <= quiz4)
         lowest = quiz3;
     else
         lowest = quiz4;
 
-    // Calculate average after dropping the lowest score
+    // Calculates average after dropping the lowest score
     float average = (quiz1 + quiz2 + quiz3 + quiz4 - lowest) / 3;
 
-    //Determine letter grade
+    //Determines letter grade
     string letterGrade;
+    string quizLetterGrade1, quizLetterGrade2, quizLetterGrade3, quizLetterGrade4;
 
-    if (format == "precent"){
+    if (format == "percent") //quiz 1 letter grade
+    {
+        if (quiz1 >= 90)
+            quizLetterGrade1 = "A";
+        else if (quiz1 <= 89 && quiz1 >= 88)
+            quizLetterGrade1 = "A-";
+        else if (quiz1 <= 87 && quiz1 >= 86 )
+            quizLetterGrade1 = "B+";
+        else if (quiz1 <= 85 && quiz1 >= 80)
+            quizLetterGrade1 = "B";
+        else if (quiz1 <= 79 && quiz1 >= 78)
+            quizLetterGrade1 = "B-";
+        else if (quiz1 <= 77 && quiz1 >= 76)
+            quizLetterGrade1 = "C+";
+        else if (quiz1 <= 75 && quiz1 >= 70)
+            quizLetterGrade1 = "C";
+        else if (quiz1 <= 70 && quiz1 >= 60)
+            quizLetterGrade1 = "D";
+        else
+            quizLetterGrade1 = "F";
+    }
+    else
+    {
+      if (quiz1 >= 9)
+            quizLetterGrade1 = "A";
+        else if (quiz1 <= 8.9 && quiz1 >= 8.8)
+            quizLetterGrade1 = "A-";
+        else if (quiz1 <= 8.7 && quiz1 >= 8.6 )
+            quizLetterGrade1 = "B+";
+        else if (quiz1 <= 8.5 && quiz1 >= 8)
+            quizLetterGrade1 = "B";
+        else if (quiz1 <= 7.9 && quiz1 >= 7.8)
+            quizLetterGrade1 = "B-";
+        else if (quiz1 <= 7.7 && quiz1 >= 7.6)
+            quizLetterGrade1 = "C+";
+        else if (quiz1 <= 7.5 && quiz1 >= 7)
+            quizLetterGrade1 = "C";
+        else if (quiz1 <= 7 && quiz1 >= 6)
+            quizLetterGrade1 = "D";
+        else
+            quizLetterGrade1 = "F";
+    }
+
+    if (format == "percent") //quiz 2 letter grade
+    {
+        if (quiz2 >= 90)
+            quizLetterGrade2 = "A";
+        else if (quiz2 <= 89 && quiz2 >= 88)
+            quizLetterGrade2 = "A-";
+        else if (quiz2 <= 87 && quiz2 >= 86 )
+            quizLetterGrade2 = "B+";
+        else if (quiz2 <= 85 && quiz2 >= 80)
+            quizLetterGrade2 = "B";
+        else if (quiz2 <= 79 && quiz2 >= 78)
+            quizLetterGrade2 = "B-";
+        else if (quiz2 <= 77 && quiz2 >= 76)
+            quizLetterGrade2 = "C+";
+        else if (quiz2 <= 75 && quiz2 >= 70)
+            quizLetterGrade2 = "C";
+        else if (quiz2 <= 70 && quiz2 >= 60)
+            quizLetterGrade2 = "D";
+        else
+            quizLetterGrade2 = "F";
+    }
+    else
+    {
+      if (quiz2 >= 9)
+            quizLetterGrade2 = "A";
+        else if (quiz2 <= 8.9 && quiz2 >= 8.8)
+            quizLetterGrade2 = "A-";
+        else if (quiz2 <= 8.7 && quiz2 >= 8.6 )
+            quizLetterGrade2 = "B+";
+        else if (quiz2 <= 8.5 && quiz2 >= 8)
+            quizLetterGrade2 = "B";
+        else if (quiz2 <= 7.9 && quiz2 >= 7.8)
+            quizLetterGrade2 = "B-";
+        else if (quiz2 <= 7.7 && quiz2 >= 7.6)
+            quizLetterGrade2 = "C+";
+        else if (quiz2 <= 7.5 && quiz2 >= 7)
+            quizLetterGrade2 = "C";
+        else if (quiz2 <= 7 && quiz2 >= 6)
+            quizLetterGrade2 = "D";
+        else
+            quizLetterGrade2 = "F";
+    }
+
+     if (format == "percent") //quiz 3 letter grade
+    {
+        if (quiz3 >= 90)
+            quizLetterGrade3 = "A";
+        else if (quiz3 <= 89 && quiz3 >= 88)
+            quizLetterGrade3 = "A-";
+        else if (quiz3 <= 87 && quiz3 >= 86 )
+            quizLetterGrade3 = "B+";
+        else if (quiz3 <= 85 && quiz3 >= 80)
+            quizLetterGrade3 = "B";
+        else if (quiz3 <= 79 && quiz3 >= 78)
+            quizLetterGrade3 = "B-";
+        else if (quiz3 <= 77 && quiz3 >= 76)
+            quizLetterGrade3 = "C+";
+        else if (quiz3 <= 75 && quiz3 >= 70)
+            quizLetterGrade3 = "C";
+        else if (quiz3 <= 70 && quiz3 >= 60)
+            quizLetterGrade3 = "D";
+        else
+            quizLetterGrade3 = "F";
+    }
+    else
+    {
+      if (quiz3 >= 9)
+            quizLetterGrade3 = "A";
+        else if (quiz3 <= 8.9 && quiz3 >= 8.8)
+            quizLetterGrade3 = "A-";
+        else if (quiz3 <= 8.7 && quiz3 >= 8.6 )
+            quizLetterGrade3 = "B+";
+        else if (quiz3 <= 8.5 && quiz3 >= 8)
+            quizLetterGrade3 = "B";
+        else if (quiz3 <= 7.9 && quiz3 >= 7.8)
+            quizLetterGrade3 = "B-";
+        else if (quiz3 <= 7.7 && quiz3 >= 7.6)
+            quizLetterGrade3 = "C+";
+        else if (quiz3 <= 7.5 && quiz3 >= 7)
+            quizLetterGrade3 = "C";
+        else if (quiz3 <= 7 && quiz3 >= 6)
+            quizLetterGrade3 = "D";
+        else
+            quizLetterGrade3 = "F";
+    }
+
+    if (format == "percent") //quiz 4 letter grade
+    {
+        if (quiz4 >= 90)
+            quizLetterGrade4 = "A";
+        else if (quiz4 <= 89 && quiz4 >= 88)
+            quizLetterGrade4 = "A-";
+        else if (quiz4 <= 87 && quiz4 >= 86 )
+            quizLetterGrade4 = "B+";
+        else if (quiz4 <= 85 && quiz4 >= 80)
+            quizLetterGrade4 = "B";
+        else if (quiz4 <= 79 && quiz4 >= 78)
+            quizLetterGrade4 = "B-";
+        else if (quiz4 <= 77 && quiz4 >= 76)
+            quizLetterGrade4 = "C+";
+        else if (quiz4 <= 75 && quiz4 >= 70)
+            quizLetterGrade4 = "C";
+        else if (quiz4 <= 70 && quiz4 >= 60)
+            quizLetterGrade4 = "D";
+        else
+            quizLetterGrade4 = "F";
+    }
+    else
+    {
+      if (quiz4 >= 9)
+            quizLetterGrade4 = "A";
+        else if (quiz4 <= 8.9 && quiz4 >= 8.8)
+            quizLetterGrade4 = "A-";
+        else if (quiz4 <= 8.7 && quiz4 >= 8.6 )
+            quizLetterGrade4 = "B+";
+        else if (quiz4 <= 8.5 && quiz4 >= 8)
+            quizLetterGrade4 = "B";
+        else if (quiz4 <= 7.9 && quiz4 >= 7.8)
+            quizLetterGrade4 = "B-";
+        else if (quiz4 <= 7.7 && quiz4 >= 7.6)
+            quizLetterGrade4 = "C+";
+        else if (quiz4 <= 7.5 && quiz4 >= 7)
+            quizLetterGrade4 = "C";
+        else if (quiz4 <= 7 && quiz4 >= 6)
+            quizLetterGrade4 = "D";
+        else
+            quizLetterGrade4 = "F";
+    }
+
+    if (format == "percent") //total letter grade
+    {
         if (average >= 90)
             letterGrade = "A";
         else if (average <= 89 && average >= 88)
@@ -53,8 +253,10 @@ int main() {
         else if (average <= 70 && average >= 60)
             letterGrade = "D";
         else
-            letterGrade = "F";}
-    else {
+            letterGrade = "F";
+    }
+    else
+    {
         if (average >= 9)
             letterGrade = "A";
         else if (average <= 8.9 && average >= 8.8)
@@ -72,31 +274,34 @@ int main() {
         else if (average <= 7 && average >= 6)
             letterGrade = "D";
         else
-            letterGrade = "F";}
-     
-    
-    // Format and display output
-    cout << fixed << setprecision(1); // Show 1 decimal places
+            letterGrade = "F";
+    }
+
+    //display output
     cout << left;
 
-    if (format == "points"){
+    if (format == "points")
+    {
         cout << "Students name: " << name << endl;
-        cout << "Grade Item" << setw(20) << "Points" << setw(20) << "Grade\n";
-        cout << "Quiz 1" << setw(20) << quiz1 << "/10" << setw(20) << letterGrade << endl;
-        cout << "Quiz 2" << setw(20) << quiz2 << "/10" << setw(20) << letterGrade << endl;
-        cout << "Quiz 3" << setw(20) << quiz3 << "/10" << setw(20) << letterGrade << endl;
-        cout << "Quiz 4" << setw(20) << quiz4 << "/10" << setw(20) << letterGrade << endl;}
-    else{
+        cout << setw(15) << "Grade Item" << setw(15) << "Grade" << setw(15) << "Points" << endl << endl;
+        cout << setw(15) << "Quiz 1" << setw(15) << quizLetterGrade1 << quiz1 << "/10" << endl;
+        cout << setw(15) << "Quiz 2" << setw(15) << quizLetterGrade2 << quiz2 << "/10" << endl;
+        cout << setw(15) << "Quiz 3" << setw(15) << quizLetterGrade3 << quiz3 << "/10" << endl;
+        cout << setw(15) << "Quiz 4" << setw(15) << quizLetterGrade4 << quiz4 << "/10" << endl << endl;
+        cout << setw(15) << "Final Grade" << setw(15) << letterGrade << fixed << setprecision(1) << average
+        << "/10" << endl;
+    }
+    else
+    {
         cout << "Students name: " << name << endl;
-        cout << "Grade Item" << setw(20) << "Precentage" << setw(20) << "Grade\n";
-        cout << "Quiz 1" << setw(20) << quiz1 << "%" << setw(20) << letterGrade << endl;
-        cout << "Quiz 2" << setw(20) << quiz2 << "%" << setw(20) << letterGrade << endl;
-        cout << "Quiz 3" << setw(20) << quiz3 << "%" << setw(20) << letterGrade << endl;
-        cout << "Quiz 4" << setw(20) << quiz4 << "%" << setw(20) << letterGrade << endl;}
-        
-    //cout << "Student Name: " << name << endl;
-    //cout << "Average (Top 3 Quizzes): " << average << "%\n";
-    //cout << "Lowest graded removed from average: " << lowest << "%\n";
-    
+        cout << setw(15) << "Grade Item" << setw(15) << "Grade" << setw(15) << "Percentage" << endl << endl;
+        cout << setw(15) << "Quiz 1" << setw(15) << quizLetterGrade1 << quiz1 << "%" << endl;
+        cout << setw(15) << "Quiz 2" << setw(15) << quizLetterGrade2 << quiz2 << "%" << endl;
+        cout << setw(15) << "Quiz 3" << setw(15) << quizLetterGrade3 << quiz3 << "%" << endl;
+        cout << setw(15) << "Quiz 4" << setw(15) << quizLetterGrade4 << quiz4 << "%" << endl << endl;
+        cout << setw(15) << "Final Grade" << setw(15) <<letterGrade << fixed << setprecision(1) << average
+        << "%" << endl;
+    }
+
     return 0;
 }
